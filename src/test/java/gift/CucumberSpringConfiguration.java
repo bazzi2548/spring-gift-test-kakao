11789b5341201c663f023d59b4c50f5dce4887a1
문정환(malang.cow) <bazzi2548@naver.com>
2026-02-24 13:32:49 +0900

    feat: Application 컨테이너화

--- src/test/java/gift/CucumberSpringConfiguration.java
+++ src/test/java/gift/CucumberSpringConfiguration.java
@@ -4,12 +4,16 @@ import io.cucumber.java.Before;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
+	@Value("${test.port:0}")
+	int testPort;
+
 	@LocalServerPort
 	int port;
 
@@ -18,7 +22,7 @@ public class CucumberSpringConfiguration {
 
 	@Before
 	public void setUp() {
-		RestAssured.port = port;
+		RestAssured.port = testPort > 0 ? testPort : port;
 		databaseCleaner.clear();
 	}
 }
